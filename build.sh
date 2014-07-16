64c71ce4b8f0fa19733266a18cbbf658cef854f9
Zhang Yungui <rhcad@hotmail.com>
2014-07-16 22:03:05 +0800

    Fix #35

--- build.sh
+++ build.sh
@@ -17,19 +17,19 @@ iphoneos51=`xcodebuild -showsdks | grep -i iphoneos5.1`
 iphoneos43=`xcodebuild -showsdks | grep -i iphoneos4.3`
 
 if [ -n "$iphoneos71" ]; then
-    xcodebuild -project TouchVG/TouchVG.xcodeproj $1 $2 -sdk iphoneos7.1 -configuration Release -alltargets
+    xcodebuild -project TouchVG.xcodeproj $1 $2 -sdk iphoneos7.1 -configuration Release -alltargets
 else
 if [ -n "$iphoneos70" ]; then
-    xcodebuild -project TouchVG/TouchVG.xcodeproj $1 $2 -sdk iphoneos7.0 -configuration Release -alltargets
+    xcodebuild -project TouchVG.xcodeproj $1 $2 -sdk iphoneos7.0 -configuration Release -alltargets
 else
 if [ -n "$iphoneos61" ]; then
-    xcodebuild -project TouchVG/TouchVG.xcodeproj $1 $2 -sdk iphoneos6.1 -configuration Release -alltargets
+    xcodebuild -project TouchVG.xcodeproj $1 $2 -sdk iphoneos6.1 -configuration Release -alltargets
 else
 if [ -n "$iphoneos51" ]; then
-    xcodebuild -project TouchVG/TouchVG.xcodeproj $1 $2 -sdk iphoneos5.1 -configuration Release
+    xcodebuild -project TouchVG.xcodeproj $1 $2 -sdk iphoneos5.1 -configuration Release
 else
 if [ -n "$iphoneos43" ]; then
-    xcodebuild -project TouchVG/TouchVG.xcodeproj $1 $2 -sdk iphoneos4.3 -configuration Release
+    xcodebuild -project TouchVG.xcodeproj $1 $2 -sdk iphoneos4.3 -configuration Release
 fi
 fi
 fi
