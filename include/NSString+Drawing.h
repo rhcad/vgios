647e8bf491ffd035ae57727768b5e0f5b0a52822
Zhang Yungui <rhcad@hotmail.com>
2014-10-10 10:57:46 +0800

    Add extern "C" in NSString+Drawing.h

--- include/NSString+Drawing.h
+++ include/NSString+Drawing.h
@@ -11,8 +11,16 @@
 #import <UIKit/UIKit.h>
 #include <CoreGraphics/CoreGraphics.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 CGSize sizeWithAttributes6(NSString *str, NSDictionary *attrs);
 void drawAtPoint6(NSString *str, CGPoint point, NSDictionary *attrs);
 void drawInRect6(NSString *str, CGRect rect, NSDictionary *attrs);
 CGRect boundingRectWithSize6(NSString *str, CGSize size, NSStringDrawingOptions options,
                              NSDictionary *attrs, NSStringDrawingContext *context);
+
+#ifdef __cplusplus
+}
+#endif
