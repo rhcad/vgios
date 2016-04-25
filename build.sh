aa1ccf12db23f5486b89eebe8e0d6697771b38b3
Zhang Yungui <rhcad@hotmail.com>
2016-04-25 14:04:49 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,12 +6,21 @@
 if [ ! -f ../vgcore/ios/build.sh ] ; then
     git clone https://github.com/rhcad/vgcore ../vgcore
 fi
-if [ ! -f ../SVGKit/SVGKit.podspec ] ; then
-    git clone https://github.com/SVGKit/SVGKit ../SVGKit
-fi
+#if [ ! -f ../SVGKit/SVGKit.podspec ] ; then
+#    git clone https://github.com/SVGKit/SVGKit ../SVGKit
+#fi
 
-xcodebuild -project TouchVG.xcodeproj $1 $2 -configuration Release -alltargets
+xcodebuild -project TouchVG.xcodeproj $1 $2 -configuration Release -target TouchVG
 
 mkdir -p output/TouchVG
-cp -R TouchVG/build/Release-universal/*.a output
-cp -R TouchVG/build/Release-universal/include/TouchVG/*.h output/TouchVG
+cp -R build/Release-universal/*.a output
+cp -R build/Release-universal/include/TouchVG/*.h output/TouchVG
+
+if [ ! -f output/libTouchVGCore.a ] ; then
+    cd ../vgcore/ios
+    sh build.sh $1 $2
+    cd ../../vgios
+fi
+mkdir -p output/TouchVGCore
+cp -R ../vgcore/ios/output/*.a output
+cp -R ../vgcore/ios/output/TouchVGCore/*.h output/TouchVGCore
